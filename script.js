220d3ae722ccc971e59ffe98807856e91e19183c
abdelmajied94abusuliman <115414116+abdelmajied94abusuliman@users.noreply.github.com>
2023-03-12 15:30:44 +0300

    Logatta Task

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeBG() {
+    document.getElementById('btn-Js').style.backgroundColor = "blue";
+}
+function returnBG() {
+    document.getElementById('btn-Js').style.backgroundColor = "#f53838";
+}
